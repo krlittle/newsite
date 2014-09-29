3313f7982681c65480c22648db6daac5cf56688d
Ken Little <klittle@shelterinsurance.com>
2014-09-29 15:31:45 -0500

    Changed background color, reworked grid to use rows, added new controllers, and took angular lesson on httprequests

---
+++ public_html/js/nameCountryController.js
@@ -0,0 +1,11 @@
+function nameCountryController($scope) {
+    $scope.nameCountry = [
+        {name:'Anna',country:'Norway'},
+        {name:'Boris',country:'Russia'},
+        {name:'Christopher',country:'Colombia'},
+        {name:'Dennis',country:'Denmark'},
+        {name:'Earl',country:'USA'},
+        {name:'Frank',country:'Germany'},
+        {name:'Gretchen',country:'Britain'}
+    ];
+}
