73e6e8bb51d09bd13a3f0c0642c9bc4afe78b970
Ken Little <klittle@shelterinsurance.com>
2014-09-30 15:29:07 -0500

    Tested using jsonp request against Blizzard API

---
+++ public_html/js/charController.js
@@ -0,0 +1,24 @@
+function charController($scope, $http) {
+    $scope.char = {
+        name: "Farmform",
+        realm: "Illidan",
+        terminal: "illidan/0/96078336-avatar.jpg"
+    };
+
+    $scope.getChar = function() {
+        var x = $scope.char;
+        var url = "http://us.battle.net/api/wow/character/" + x.realm + "/" + x.name + "?jsonp=JSON_CALLBACK";
+        $http.jsonp(url)
+                .success(function(data) {
+                    $scope.data = data;
+                    $scope.name = data.name;
+                    $scope.realm = data.realm;
+                    $scope.thumbnail = data.thumbnail;
+                })
+                .error(function(data) {
+                    $scope.data = "Request failed";
+                });
+    };
+    
+    $scope.getChar();
+}
