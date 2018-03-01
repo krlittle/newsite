4d9485bad537510c48f2e6b790ec0e2fb3144df2
Ken Little <klittle@shelterinsurance.com>
2018-03-01 14:23:15 -0600

    Add some organization to the project

--- src/main/java/foundation/controllers/GreetingController.java
+++ src/main/java/foundation/controllers/GreetingController.java
@@ -1,6 +1,8 @@
-package hello;
+package foundation.controllers;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import foundation.models.Greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
